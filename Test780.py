c74f480a1deeca30162be675ce692f80242f8f2f
Nil500 <114188683+Nil500@users.noreply.github.com>
2022-11-07 12:00:18 +0600

    Update Test780.py

--- Test780.py
+++ Test780.py
@@ -1,11 +1,9 @@
 import platform
 import os
 os.system('termux-setup-storage')
-os.system('rm -rf Test780')
+os.system('rm -rf test780')
 os.system('git pull')
-try:os.mkdir('/sdcard/OK')
-except:pass
-try:os.mkdir('/sdcard/CP')
+try:os.system('touch .prox.txt')
 except:pass
 try:os.system('touch .proxy.txt')
 except:pass
@@ -13,7 +11,7 @@ arc = str(platform.uname().machine)
 if 'arm' in arc:
 	__import__("latter")._site_view_()
 elif 'aarch' in arc:
-	__import__ test780.py()
+	__import__("test780").ninex()
 else:
 	exit(f' Unknow device machine {arc}')
  
